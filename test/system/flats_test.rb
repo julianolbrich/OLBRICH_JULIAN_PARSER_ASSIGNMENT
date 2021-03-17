c845e29d3f2879aad4713eac649edd9bef393aa3
Julian Olbrich <julianolbrich@gmail.com>
2021-03-17 16:00:14 +0100

    added gem 'launchy' + set up first Minitest while using screenshots to inspect

--- test/system/flats_test.rb
+++ test/system/flats_test.rb
@@ -1,53 +1,48 @@
 require "application_system_test_case"
 
 class FlatsTest < ApplicationSystemTestCase
-  setup do
-    @flat = flats(:one)
+
+#   test "something on object" do
+#     # setup     -> i.e. have models in place
+#     # exercise  -> do the thing that you want to execute
+#     # verify    -> where you compare two things, is this what i expected?
+#     # teardown  -> remove tests that you don't us
+#   end
+
+  test "visiting the home page with flats displayed" do
+    visit root_path
+    save_and_open_screenshot
+
+    assert_selector "h5", text: "HOMEDAY ASSIGNMENT"
   end
 
-  test "visiting the index" do
-    visit flats_url
-    assert_selector "h1", text: "Flats"
-  end
+  # test "visiting the index" do
+  #   visit root_path
+  #   assert_selector "h5", text: "Flats"
+  # end
 
-  test "creating a Flat" do
-    visit flats_url
-    click_on "New Flat"
+  # test "creating a Flat" do
+  #   visit flats_url
+  #   click_on "New Flat"
 
-    fill_in "City", with: @flat.city
-    fill_in "Country", with: @flat.country
-    fill_in "District", with: @flat.district
-    fill_in "State", with: @flat.state
-    fill_in "Street", with: @flat.street
-    fill_in "Street number", with: @flat.street_number
-    click_on "Create Flat"
+  #   fill_in "City", with: @flat.city
+  #   fill_in "Country", with: @flat.country
+  #   fill_in "District", with: @flat.district
+  #   fill_in "State", with: @flat.state
+  #   fill_in "Street", with: @flat.street
+  #   fill_in "Street number", with: @flat.street_number
+  #   click_on "Create Flat"
 
-    assert_text "Flat was successfully created"
-    click_on "Back"
-  end
+  #   assert_text "Flat was successfully created"
+  #   click_on "Back"
+  # end
 
-  test "updating a Flat" do
-    visit flats_url
-    click_on "Edit", match: :first
+  # test "destroying a Flat" do
+  #   visit flats_url
+  #   page.accept_confirm do
+  #     click_on "Delete", match: :first
+  #   end
 
-    fill_in "City", with: @flat.city
-    fill_in "Country", with: @flat.country
-    fill_in "District", with: @flat.district
-    fill_in "State", with: @flat.state
-    fill_in "Street", with: @flat.street
-    fill_in "Street number", with: @flat.street_number
-    click_on "Update Flat"
-
-    assert_text "Flat was successfully updated"
-    click_on "Back"
-  end
-
-  test "destroying a Flat" do
-    visit flats_url
-    page.accept_confirm do
-      click_on "Destroy", match: :first
-    end
-
-    assert_text "Flat was successfully destroyed"
-  end
+  #   assert_text "Flat was successfully destroyed"
+  # end
 end
